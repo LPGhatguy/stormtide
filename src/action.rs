e5437f3b1d3d144d0341d044c8831879df5eccf9
Lucien Greathouse <me@lpghatguy.com>
2020-08-24 11:56:16 -0700

    Flesh out Action

--- src/action.rs
+++ src/action.rs
@@ -1,5 +1,7 @@
 //! Describes an action that a player can (potentially) take in the game.
 
+use hecs::Entity;
+
 #[derive(Debug)]
 pub enum Action {
     /// 104.3a A player can concede the game at any time. A player who concedes
@@ -7,4 +9,33 @@ pub enum Action {
     Concede,
 
     PassPriority,
+
+    /// 117.1a A player may cast an instant spell any time they have priority. A
+    ///        player may cast a noninstant spell during their main phase any
+    ///        time they have priority and the stack is empty.
+    CastSpell {
+        spell: Entity,
+        // TODO: Targets
+        // TODO: Costs
+    },
+
+    /// 117.1b A player may activate an activated ability any time they have
+    ///        priority.
+    ActivateAbility {
+        object: Entity,
+        ability: usize,
+        // TODO: Targets
+        // TODO: Costs
+    },
+
+    /// 116.2a Playing a land is a special action. To play a land, a player puts
+    ///        that land onto the battlefield from the zone it was in (usually
+    ///        that player’s hand). By default, a player can take this action
+    ///        only once during each of their turns. A player can take this
+    ///        action any time they have priority and the stack is empty during
+    ///        a main phase of their turn. See rule 305, “Lands.”
+    PlayLand {
+        card: Entity,
+    },
+    // TODO: Rules 116.2b—116.2i
 }
